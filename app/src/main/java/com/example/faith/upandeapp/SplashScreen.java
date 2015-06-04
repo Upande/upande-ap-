f46b74f398b62b3b81d6900aa4e7b9c3b50ac496
Faith Muli <faithmuli33@gmail.com>
2015-06-04 13:13:49 +0300

    splashscreen done

--- app/src/main/java/com/example/faith/upandeapp/SplashScreen.java
+++ app/src/main/java/com/example/faith/upandeapp/SplashScreen.java
@@ -1,17 +1,33 @@
 package com.example.faith.upandeapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Handler;
 
 
-public class SplashScreen extends ActionBarActivity {
 
+
+public class SplashScreen extends Activity{
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+            new android.os.Handler().postDelayed(new Runnable(){
+
+
+                @Override
+                public void run() {
+                    Intent mainIntent= new Intent(SplashScreen.this, InfoPage.class);
+                    SplashScreen.this.startActivity(mainIntent);
+                    SplashScreen.this.finish();
+                }
+            },SPLASH_DISPLAY_LENGTH);
 
 
     }
