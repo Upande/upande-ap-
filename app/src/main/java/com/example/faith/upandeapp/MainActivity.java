8f8a986954fa2f32f57b0bcbebfb7f383d221cf8
justus <jmbaluka@mfarm.co.ke>
2015-06-04 18:11:26 +0300

    added main activity content prototype

--- app/src/main/java/com/example/faith/upandeapp/MainActivity.java
+++ app/src/main/java/com/example/faith/upandeapp/MainActivity.java
@@ -1,38 +1,35 @@
 package com.example.faith.upandeapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runNextTask();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void runNextTask(){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        //set up for model selection
+        TextView modelTextview = (TextView)findViewById(R.id.model);
+        modelTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),FormPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
