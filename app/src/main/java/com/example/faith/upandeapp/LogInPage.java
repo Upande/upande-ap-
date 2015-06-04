359dfd0c0fc5ce1995f93197a31351f800366b16
justus <jmbaluka@mfarm.co.ke>
2015-06-04 15:35:08 +0300

    changed background and added onclick listenters and their intentions to the buttons

--- app/src/main/java/com/example/faith/upandeapp/LogInPage.java
+++ app/src/main/java/com/example/faith/upandeapp/LogInPage.java
@@ -1,17 +1,30 @@
 package com.example.faith.upandeapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class LogInPage extends ActionBarActivity {
-
+public class LogInPage extends Activity {
+   Button goToProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
+        goToProfile = (Button)findViewById(R.id.button2);
+        goToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
