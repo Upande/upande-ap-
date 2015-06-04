9c8079530c2e036786c90c337c7e94ebc2ad930b
justus <jmbaluka@mfarm.co.ke>
2015-06-04 13:15:38 +0300

    designed info page

--- app/src/main/java/com/example/faith/upandeapp/InfoPage.java
+++ app/src/main/java/com/example/faith/upandeapp/InfoPage.java
@@ -1,12 +1,13 @@
 package com.example.faith.upandeapp;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class InfoPage extends ActionBarActivity {
+public class InfoPage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +15,5 @@ public class InfoPage extends ActionBarActivity {
         setContentView(R.layout.activity_info_page);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_info_page, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
